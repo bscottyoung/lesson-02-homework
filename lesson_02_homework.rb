d393aee8bc22288951dfcf62e2b5224d46e5fd71
bscottyoung <brian.youngatl@gmail.com>
2016-02-22 16:19:11 -0500

    HW #2 for Brian Young

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -1,9 +1,11 @@
 # Code Reading
 
 def fizzbuzz(num)
-  # Example comment line
+#bsy> create new method passing 'num' as argument
 
   case
+#bsy> a type of a loop but its incomplete...it should be 'case num'
+#bsy> when num can be divided eaually by 15, 3, or 5...assign the relevant variable
     when num % 15 == 0 then "FizzBuzz"
     when num % 3  == 0 then "Fizz"
     when num % 5  == 0 then "Buzz"
@@ -12,24 +14,73 @@ def fizzbuzz(num)
 end
 
 def fizz_buzz_to(limit)
+#bsy> create new method with 'limit' as argument
   1.upto(limit).each do |num|
+#bsy> up to the 'limit' call the above method (fizzbuzz) and output to console the value    
     puts fizzbuzz(num)
   end
 end
 
 # Fix Broken Code
 
-def area_of_triangle(b, h)
-  puts b x height / 2
+def area_of_triangle(base, height)
+  puts base * height / 2
 end
 
 base = 7
 height = 6
-
-puts area_of_triangle(height, height)
+puts area_of_triangle(base, height)
 
 # Coding
 
 class Waitlist
-  # add your code here
+  attr_accessor :add_party_name, :list
+  ##> Initialize causes errors....Why???
+  # def initialize
+  #   @add_party_name = add_party_name
+  #   @list  = list 
+  #   @list1 = list1  
+  # end
+
+# =>add_party works
+  def add_party(add_party_name, list)
+    list << add_party_name
+    puts "Updated waitlist:"
+    list.each do |name|
+      puts name
+    end
+  end
+
+# =>list  NOT working
+  def list(list)
+    puts "Current list:" 
+    list.each do |name|
+      puts name
+    end
+  end
+
+# =>seat works
+  def seat(list)
+    puts "Party Seated: #{list.shift}"
+    puts ""
+    puts "Updated list:" 
+    list.each do |name|
+      puts name
+    end
+  end
+
+
+#class end
 end
+
+list = ["kareem grant", "brian young", "ted dansen", 'helga horchow' ]
+
+#add_party_name to waitlist
+add_party_name="lawrence olivier"
+Waitlist.new.add_party(add_party_name, list)
+
+#output the waiting list
+Waitlist.new.list(list)
+
+#seat the fist party and output the updated list
+Waitlist.new.seat(list)
