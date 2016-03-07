bb41f1cd620f743530da14923e5bb6945881fe6c
Brian Young <brian.youngatl@gmail.com>
2016-03-06 19:27:46 -0500

    HW2-removed extraneous comments - young

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -59,7 +59,7 @@ class Waitlist
 end
 
 
-# =>                      ** FOR HOMEWORK: ** 
+
 #Method: add_party 
 list=Waitlist.new
 add_party_name = "Ted"
@@ -73,73 +73,3 @@ puts "Current Waitlist: #{list.list}"
 list=Waitlist.new
 puts "Updated list after first person seated: #{list.seat}"
 
-# ----------------------END OF HOMEWORK-----------------------------
-
-
-# ----------------------BEGIN MY APPROACH-----------------------------
-# =>MY APPROACH...allows me to use the list for other operations **
-
-# class Waitlist
-#   attr_writer :output
-#   attr_reader :list
-
-#   def initialize 
-#       @list=["Kareem", "Brian"]
-#   end
-
-# => METHOD: list
-  # def list   
-  #   output[:list_array]=@list
-  # end
-
-# => METHOD: add_party
-  # def add_party(output)
-  #   puts "enter party to be added to waitlist:"
-  #   add_party_name = gets.chomp
-  #   @list << add_party_name
-  #   output[:name] = add_party_name
-  #   output[:list_array] = @list
-  #   return output
-  # end
-
-# => METHOD: seat 
-  # def seat(output)
-  #   output[:seated] = @list.shift
-  #   output[:list_array] = @list    
-  #   return output
-  # end
-# end
-
-
-
-# => CALL add_party
-# list = Waitlist.new
-# output={}
-# list.add_party(output)
-# puts "New party added to waitlist: #{output[:name]}"
-# puts "Do you want to add another name? (Y or N)"
-# ans=gets.capitalize.chomp
-# while ans=="Y"  do
-#   list.add_party(output)
-#   puts "New party added to waitlist: #{output[:name]}"
-#   puts "Do you want to add another name? (Y or N)"  
-#   ans=gets.capitalize.chomp
-# end
-# puts "\nUpdated Waitlist: "
-# output[:list_array].each {|name| puts name}
-# puts ""
-#-----------------------------------------------------
-
-# => CALL list
-#list=Waitlist.new
-# puts "current waiting list:"
-# list.list(output)
-#puts output[:array_list].each {|name| puts name}
-#-----------------------------------------------------
-
-# => CALL seat
-# list = Waitlist.new
-# list.seat(output)
-# puts "\nPerson seated: #{output[:seated]}"
-# puts "Updated waiting list:"
-# output[:list_array].each {|name| puts name}
